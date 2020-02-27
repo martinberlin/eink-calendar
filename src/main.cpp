59e08ab5b3b12d858cf2cc5509bead3b37b5407c
martinberlin <martin@luckycloud.de>
2020-02-27 17:31:53 +0100

    #7 Remove Server and mDns, leaving only the essential to send a bitmap to the display

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,9 @@
 #ifdef ESP32
   #include <WiFi.h>
   #include <ESPmDNS.h>
-  #include <WebServer.h>
 #elif ESP8266
   #include <ESP8266WiFi.h>
   #include <ESP8266mDNS.h>
-  #include <ESP8266WebServer.h>
   #include <DNSServer.h>
 #endif
 #include <WiFiClient.h>
@@ -27,17 +25,7 @@ bool debugMode = false;
 
 unsigned int secondsToDeepsleep = 0;
 uint64_t USEC = 1000000;
-const char* domainName = "calendar"; // mDNS
-String message;
-// Makes a div id="m" containing response message to dissapear after 3 seconds
-String javascriptFadeMessage = "<script>setTimeout(function(){document.getElementById('m').innerHTML='';},3000);</script>";
-
-// TCP server at port 80 will respond to HTTP requests
-#ifdef ESP32
-  WebServer server(80);
-  #elif ESP8266
-  ESP8266WebServer server(80);
-#endif
+//String message;
 
 // SPI interface GPIOs defined in Config.h  
 GxIO_Class io(SPI, EINK_CS, EINK_DC, EINK_RST);
@@ -57,100 +45,9 @@ void displayMessage(String message, int height) {
   display.update(); // -> Since could not make partial updateWindow work
 }
 
-void handle_http_not_found() {
-  server.send(404, "text/plain", "Not Found");
-}
-
-void handle_http_root() {
-
-  String headers = "<head><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\">";
-  headers += "<meta name='viewport' content='width=device-width,initial-scale=1'></head>";
-  String html = "<body><main role='main'><div class='container-fluid'><div class='row'>";
-  html += "<div class='col-md-12'><h4>" + String(domainName) + ".local</h4>";
-  html += "<form id='f' action='/web-image' target='frame' method='POST'>";
-  html += "<div class='row'><div class='col-sm-12'>";
-
-  html += "<select name='zoom' style='width:7em' class='form-control float-right'>";
-  html += "<option value='2'>2</option>";
-  html += "<option value='1.7'>1.7</option>";
-  html += "<option value='1.5'>1.5</option>";
-  html += "<option value='1.4'>1.4</option>";
-  html += "<option value='1.3'>1.3</option>";
-  html += "<option value='1.2'>1.2</option>";
-  html += "<option value='1.1'>1.1 10% zoomed</option>";
-  html += "<option value='' selected>Zoom</option>";
-  html += "<option value='1'>1 no zoom</option>";
-  html += "<option value='.9'>.9 10% smaller</option>";
-  html += "<option value='.85'>.85</option>";
-  html += "<option value='.8'>.8</option>";
-  html += "<option value='.7'>.7</option>";
-  html += "<option value='.6'>.6</option>";
-  html += "<option value='.5'>.5 half size</option></select>&nbsp;";
-  
-  html += "<select name='brightness' style='width:8em' class='form-control float-right'>";
-  html += "<option value='150'>+5</option>";
-  html += "<option value='140'>+4</option>";
-  html += "<option value='130'>+3</option>";
-  html += "<option value='120'>+2</option>";
-  html += "<option value='110'>+1</option>";
-  html += "<option value='' selected>Brightness</option>";
-  html += "<option value='90'>-1</option>";
-  html += "</select>";
-
-  html += "</div></div>";
-  html += "<input placeholder='http://' id='url' name='url' type='url' class='form-control' value='"+calendarUrl+"'>";
-  html += "<div class='row'><div class='col-sm-12 form-group'>";
-  html += "<input type='submit' value='Website screenshot' class='btn btn-mini btn-dark'>&nbsp;";
-  html += "<input type='button' onclick='document.getElementById(\"url\").value=\"\"' value='Clean url' class='btn btn-mini btn-default'></div>";
-  html += "</div></form>";
-  
-  html += "<form id='f2' action='/display-write' target='frame' method='POST'>";
-  html += "<label for='title'>Title:</label><input id='title' name='title' class='form-control'><br>";
-  html += "<textarea placeholder='Content' name='text' rows=4 class='form-control'></textarea>";
-  html += "<input type='submit' value='Send to display' class='btn btn-success'></form>";
-  html += "<a class='btn btn-default' role='button' target='frame' href='/display-clean'>Clean screen</a><br>";
-  html += "<iframe name='frame'></iframe>";
-  html += "<a href='/deep-sleep' target='frame'>Deep sleep</a><br>";
-  html += "</div></div></div></main>";
-  html += "</body>";
-
-  server.send(200, "text/html", headers + html);
-}
-
-void handleDeepSleep() {
-  server.send(200, "text/html", "Going to deep-sleep. Reset to wake up");
-  delay(1);
-  ESP.deepSleep(20e6);
-}
-
-
-void handleDisplayClean() {
+void displayClean() {
   display.fillScreen(GxEPD_WHITE);
   display.update();
-  server.send(200, "text/html", "Clearing display");
-}
-
-void handleDisplayWrite() {
-  display.fillScreen(GxEPD_WHITE);
-
-  // Analizo el POST iterando cada value
-  if (server.args() > 0) {
-    for (byte i = 0; i < server.args(); i++) {
-
-      if (server.argName(i) == "title") {
-        display.setFont(&FreeMonoBold24pt7b);
-        display.setCursor(10, 43);
-        display.print(server.arg(i));
-      }
-      if (server.argName(i) == "text") {
-        display.setFont(&FreeMonoBold12pt7b);
-        display.setCursor(10, 75);
-        display.print(server.arg(i));
-      }
-    }
-  }
-  display.update();
-  server.send(200, "text/html", "Text sent to display");
 }
 
 uint16_t read16()
@@ -174,38 +71,29 @@ uint32_t read32()
   return result;
 }
 
+char * hostFrom(char url[]) {
+  char * pch;
+  pch = strtok (url,"/");
+  __uint8_t p = 0;
+  while (pch != NULL && p<2)
+  {
+    if (p==1) break;
+    pch = strtok (NULL, "/");
+    p++;
+  }
+  return pch;
+}
 
 void handleWebToDisplay() {
   int millisIni = millis();
-  String url = calendarUrl;
-  String zoom = DEFAULT_ZOOM;
-  String brightness = "100";
-  if (server.args() > 0) {
-    for (byte i = 0; i < server.args(); i++) {
-      if (server.argName(i) == "url" && server.arg(i) != "") {
-        url = server.arg(i);
-      }
-      if (server.argName(i) == "zoom" && server.arg(i) != "") {
-        zoom = server.arg(i);
-      }
-      if (server.argName(i) == "brightness" && server.arg(i) != "") {
-        brightness = server.arg(i);
-      }
-    }
-  }
-    if (url == "") {
-      display.println("No Url received");
-      display.update();
-      return;
-    }
-  
-  String image = screenshotPath+"?u=" + url + "&z=" + zoom + "&b=" + brightness + "&eink=" + EINKMODEL;
+  char * host = hostFrom(screenUrl);
+
   String request;
-  request  = "GET " + image + " HTTP/1.1\r\n";
+  request  = "GET " + screenPath + " HTTP/1.1\r\n";
   request += "Host: " + String(screenshotHost) + "\r\n";
   request += "Connection: close\r\n";
   request += "\r\n";
-  Serial.println(String(screenshotHost)+image);
+  //Serial.println(String(host)+screenPath);
 
   client.connect(screenshotHost, 80);
   client.print(request); //send the http request to the server
@@ -325,9 +213,8 @@ while (client.available()) {
         } // end pixel
       } // end line
       int millisEnd = millis();
-       server.send(200, "text/html", "<div id='m'>Image sent to display</div>"+javascriptFadeMessage);
 
-       Serial.printf("Bytes read: %lu BMP headers detected: %d ms. BMP total fetch: %d ms.  Total download: %d ms\n",bytesRead,millisBmp-millisIni, millisEnd-millisBmp, millisEnd-millisIni);
+      Serial.printf("Bytes read: %lu BMP headers detected: %d ms. BMP total fetch: %d ms.  Total download: %d ms\n",bytesRead,millisBmp-millisIni, millisEnd-millisBmp, millisEnd-millisIni);
 
        display.update();
        Serial.printf("display.update() render: %lu ms.\n", millis()-millisEnd);
@@ -335,7 +222,6 @@ while (client.available()) {
        break;
        
     } else {
-      server.send(200, "text/html", "<div id='m'>Unsupported image format (depth:"+String(depth)+")</div>"+javascriptFadeMessage);
       display.setCursor(10, 43);
       display.print("Compressed BMP files are not handled. Unsupported image format (depth:"+String(depth)+")");
       display.update();
@@ -347,7 +233,6 @@ while (client.available()) {
 }
 
 void loop() {
-  server.handleClient();
 
   // Note: Enable deepsleep only as last step when all the rest is working as you expect
 #ifdef DEEPSLEEP_ENABLED
@@ -371,6 +256,9 @@ void loop() {
 
 void setup() {
   Serial.begin(115200);
+  
+  char * host = hostFrom(screenUrl);
+  Serial.printf("HOST: %s \n", host); // This is OK
 
   display.init();
   display.setRotation(eink_rotation); // Rotates display N times clockwise
@@ -378,41 +266,21 @@ void setup() {
   display.setTextColor(GxEPD_BLACK);
   uint8_t connectTries = 0;
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED && connectTries<10) {
+  while (WiFi.status() != WL_CONNECTED && connectTries<20) {
     Serial.print(" .");
     delay(500);
     connectTries++;
   }
-Serial.println(WiFi.localIP());
 
-  if (!MDNS.begin(domainName)) {
-    Serial.println("Error setting up MDNS responder!");
-    while (1) {
-      delay(1000);
-    }
-  }
-  Serial.printf("mDNS responder: %s.local\n",domainName);
-  // Add service to MDNS-SD
-  MDNS.addService("http", "tcp", 80);
-  
-  // Start HTTP server
-  server.onNotFound(handle_http_not_found);
-  // Routing
-  server.on("/", handle_http_root);
-  server.on("/display-write", handleDisplayWrite);
-  server.on("/web-image", handleWebToDisplay);
-  server.on("/display-clean", handleDisplayClean);
-  server.on("/deep-sleep", handleDeepSleep);
-  server.begin();
-  delay(50);
+
   if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("ONLINE");
+    Serial.println(WiFi.localIP());
+    delay(999);
     handleWebToDisplay();
   } else {
-    // Just restart fast do not print in display
-    /* 
-    displayMessage("Please check your credentials in Config.h\nCould not connect to "+String(WIFI_SSID),80);
-    Serial.printf("Please check your credentials in Config.h\nCould not connect to %s\n",WIFI_SSID);
-    delay(1000); */
-    ESP.restart();
-  }
+    Serial.printf("Going to sleep %d seconds\n", 120);
+    esp_sleep_enable_timer_wakeup(120 * USEC);
+    esp_deep_sleep_start();
+      }
 }
