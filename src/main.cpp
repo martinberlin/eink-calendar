a6156e621ffcf1fe10ea6fc24d4c198a9832c784
martinberlin <martin@luckycloud.de>
2020-03-19 11:52:38 +0100

    #16 start cleaning up and adding Bodmer libraries

--- src/main.cpp
+++ src/main.cpp
@@ -9,54 +9,13 @@
 #endif
 #include <WiFiClient.h>
 #include <SPI.h>
-#include <GxEPD.h>
+// JPEG decoder library
+#include <JPEGDecoder.h>
+#include <TFT_eSPI.h>
 
-// Get the right interface for the display
-#ifdef GDEW042T2
-  #include <GxGDEW042T2/GxGDEW042T2.h>
-  #elif defined(GDEW075T8)
-  #include <GxGDEW075T8/GxGDEW075T8.h>
-  #elif defined(GDEW075T7)
-  #include <GxGDEW075T7/GxGDEW075T7.h> 
-  #elif defined(GDEW0213I5F)
-  #include <GxGDEW0213I5F/GxGDEW0213I5F.h>
-  #elif defined(GDE0213B1)
-  #include <GxGDE0213B1/GxGDE0213B1.h>
-  #elif defined(GDEH0213B72)
-  #include <GxGDEH0213B72/GxGDEH0213B72.h>
-  #elif defined(GDEH0213B73)
-  #include <GxGDEH0213B73/GxGDEH0213B73.h>
-  #elif defined(GDEW0213Z16)
-  #include <GxGDEW0213Z16/GxGDEW0213Z16.h>
-  #elif defined(GDEH029A1)
-  #include <GxGDEH029A1/GxGDEH029A1.h>
-  #elif defined(GDEW029T5)
-  #include <GxGDEW029T5/GxGDEW029T5.h>
-  #elif defined(GDEW029Z10)
-  #include <GxGDEW029Z10/GxGDEW029Z10.h>
-  #elif defined(GDEW026T0)
-  #include <GxGDEW026T0/GxGDEW026T0.h>
-  #elif defined(GDEW027C44)
-  #include <GxGDEW027C44/GxGDEW027C44.h>
-  #elif defined(GDEW027W3)
-  #include <GxGDEW027W3/GxGDEW027W3.h>
-  #elif defined(GDEW0371W7)
-  #include <GxGDEW0371W7/GxGDEW0371W7.h>
-  #elif defined(GDEW042Z15)
-  #include <GxGDEW042Z15/GxGDEW042Z15.h>
-  #elif defined(GDEW0583T7)
-  #include <GxGDEW0583T7/GxGDEW0583T7.h>
-  #elif defined(GDEW075Z09)
-  #include <GxGDEW075Z09/GxGDEW075Z09.h>
-  #elif defined(GDEW075Z08)
-  #include <GxGDEW075Z08/GxGDEW075Z08.h>
-#endif
-
-#include <GxIO/GxIO_SPI/GxIO_SPI.cpp>
-#include <GxIO/GxIO.cpp>
-
+TFT_eSPI tft = TFT_eSPI();
 // FONT used for title / message body
-//Converting fonts with ümlauts: ./fontconvert *.ttf 18 32 252
+// Converting fonts with ümlauts: ./fontconvert *.ttf 18 32 252
 #include <Fonts/FreeMonoBold12pt7b.h>
 #include <Fonts/FreeMonoBold24pt7b.h>
 
@@ -64,29 +23,8 @@ bool debugMode = false;
 
 unsigned int secondsToDeepsleep = 0;
 uint64_t USEC = 1000000;
-
-// SPI interface GPIOs defined in Config.h  
-GxIO_Class io(SPI, EINK_CS, EINK_DC, EINK_RST);
-// (GxIO& io, uint8_t rst = D4, uint8_t busy = D2);
-GxEPD_Class display(io, EINK_RST, EINK_BUSY );
-
 WiFiClient client; // wifi client object
 
-// Displays message doing a partial update
-void displayMessage(String message, int height) {
-  Serial.println("DISPLAY prints: "+message);
-  display.setTextColor(GxEPD_WHITE);
-  display.fillRect(0,0,display.width(),height,GxEPD_BLACK);
-  display.setCursor(2, 25);
-  display.print(message);
-  display.updateWindow(0,0,display.width(),height, true); // Attempt partial update
-  display.update(); // -> Since could not make partial updateWindow work
-}
-
-void displayClean() {
-  display.fillScreen(GxEPD_WHITE);
-  display.update();
-}
 
 uint16_t read16()
 {
@@ -200,8 +138,7 @@ void handleWebToDisplay() {
   client.connect(host, 80);
   client.print(request); //send the http request to the server
   client.flush();
-  display.fillScreen(GxEPD_WHITE);
-  
+
   unsigned long timeout = millis();
   while (client.available() == 0) {
     if (millis() - timeout > 5000) {
@@ -210,136 +147,20 @@ void handleWebToDisplay() {
       return;
     }
   }
+  // Read image and dump it on the TFT 
   
-  int displayWidth = display.width();
-  int displayHeight= display.height();// Not used now
-  uint8_t buffer[displayWidth]; // pixel buffer, size for r,g,b
-  long bytesRead = 34; // summing the whole BMP info headers
-  long count = 0;
-  uint8_t lastByte = 0x00;
-
 // Start reading bits
 while (client.available()) {
-  count++;
-  uint8_t clientByte = client.read();
-  uint16_t bmp;
-  ((uint8_t *)&bmp)[0] = lastByte; // LSB
-  ((uint8_t *)&bmp)[1] = clientByte; // MSB
-  if (debugMode) {
-    Serial.print(bmp,HEX);Serial.print(" ");
-    if (0 == count % 16) {
-      Serial.println();
-    }
-    delay(1);
-  }
-  lastByte = clientByte;
-  
-  if (bmp == 0x4D42) { // BMP signature
-    int millisBmp = millis();
-    uint32_t fileSize = read32();
-    read32(); // creatorBytes
-    uint32_t imageOffset = read32(); // Start of image data
-    uint32_t headerSize = read32();
-    uint32_t width  = read32();
-    uint32_t height = read32();
-    uint16_t planes = read16();
-    uint16_t depth = read16(); // bits per pixel
-    uint32_t format = read32();
-    
-      Serial.print("->BMP starts here. File size: "); Serial.println(fileSize);
-      Serial.print("Image Offset: "); Serial.println(imageOffset);
-      Serial.print("Header size: "); Serial.println(headerSize);
-      Serial.print("Width * Height: "); Serial.print(String(width) + " x " + String(height));
-      Serial.print(" / Bit Depth: "); Serial.println(depth);
-      Serial.print("Planes: "); Serial.println(planes);Serial.print("Format: "); Serial.println(format);
-    
-    if ((planes == 1) && (format == 0 || format == 3)) { // uncompressed is handled
-      // Attempt to move pointer where image starts
-      client.readBytes(buffer, imageOffset-bytesRead); 
-      size_t buffidx = sizeof(buffer); // force buffer load
-      
-      for (uint16_t row = 0; row < height; row++) // for each line
-      {
-        //delay(1); // May help to avoid Wdt reset
-        uint8_t bits = 0;
-        for (uint16_t col = 0; col < width; col++) // for each pixel
-        {
-          yield();
-          // Time to read more pixel data?
-          if (buffidx >= sizeof(buffer))
-          {
-            client.readBytes(buffer, sizeof(buffer));
-            buffidx = 0; // Set index to beginning
-            //Serial.printf("ReadBuffer Row: %d bytesRead: %d\n",row,bytesRead);
-          }
-          switch (depth)
-          {
-            case 1: // one bit per pixel b/w format
-              {
-                if (0 == col % 8)
-                {
-                  bits = buffer[buffidx++];
-                  bytesRead++;
-                }
-                uint16_t bw_color = bits & 0x80 ? GxEPD_WHITE : GxEPD_BLACK;
-                display.drawPixel(col, displayHeight-row, bw_color);
-                bits <<= 1;
-              }
-              break;
-              
-            case 4: // was a hard work to get here
-              {
-                if (0 == col % 2) {
-                  bits = buffer[buffidx++];
-                  bytesRead++;
-                }   
-                bits <<= 1;           
-                bits <<= 1;
-                uint16_t bw_color = bits > 0x80 ? GxEPD_WHITE : GxEPD_BLACK;
-                display.drawPixel(col, displayHeight-row, bw_color);
-                bits <<= 1;
-                bits <<= 1;
-              }
-              break;
-              
-             case 24: // standard BMP format
-              {
-                uint16_t b = buffer[buffidx++];
-                uint16_t g = buffer[buffidx++];
-                uint16_t r = buffer[buffidx++];
-                uint16_t bw_color = ((r + g + b) / 3 > 0xFF  / 2) ? GxEPD_WHITE : GxEPD_BLACK;
-                display.drawPixel(col, displayHeight-row, bw_color);
-                bytesRead = bytesRead +3;
-              }
-          }
-        } // end pixel
-      } // end line
-      int millisEnd = millis();
-
-      Serial.printf("Bytes read: %lu BMP headers detected: %d ms. BMP total fetch: %d ms.  Total download: %d ms\n",bytesRead,millisBmp-millisIni, millisEnd-millisBmp, millisEnd-millisIni);
-
-       display.update();
-       Serial.printf("display.update() render: %lu ms.\n", millis()-millisEnd);
-       client.stop();
-       break;
-       
-    } else {
-      display.setCursor(10, 43);
-      display.print("Compressed BMP files are not handled. Unsupported image format (depth:"+String(depth)+")");
-      display.update();
-      
-    }
-  }
-  
+    yield();
   }     
 }
+
 
 void loop() {
 
   // Note: Enable deepsleep only as last step when all the rest is working as you expect
 #ifdef DEEPSLEEP_ENABLED
   if (secondsToDeepsleep>SLEEP_AFTER_SECONDS) {
-      display.powerDown();
       delay(10);
       #ifdef ESP32
         Serial.printf("Going to sleep %llu seconds\n", DEEPSLEEP_SECONDS);
@@ -359,10 +180,6 @@ void loop() {
 void setup() {
   Serial.begin(115200);
 
-  display.init();
-  display.setRotation(eink_rotation); // Rotates display N times clockwise
-  display.setFont(&FreeMonoBold12pt7b);
-  display.setTextColor(GxEPD_BLACK);
   uint8_t connectTries = 0;
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED && connectTries<6) {
@@ -370,7 +187,6 @@ void setup() {
     delay(500);
     connectTries++;
   }
-
 
   if (WiFi.status() == WL_CONNECTED) {
     Serial.println("ONLINE");
@@ -381,8 +197,7 @@ void setup() {
     // There is no WiFi or can't connect. After getting this to work leave this at least in 600 seconds so it will retry in 10 minutes so 
     //                                    if your WiFi is temporarily down the system will not drain your battery in a loop trying to connect.
     int secs = 10;
-    display.powerDown();
-
+    
       #ifdef ESP32
         Serial.printf("Going to sleep %d seconds\n", secs);
         esp_sleep_enable_timer_wakeup(secs * USEC);
